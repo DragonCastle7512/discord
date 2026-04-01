6ecb5d16e409a98963ceb0ab4e7ab20faa75f172
DragonCastle7512 <kimys75128386@gmail.com>
2026-04-01 22:11:31 +0900

    chore: Oracle Database TLS 방식으로 변경
    - Wallet 기반 접속 -> TLS기반으로 교체

--- db/sequelize.js
+++ db/sequelize.js
@@ -7,13 +7,9 @@ const sequelize = new Sequelize({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME || '',
-  host: process.env.DB_CONNECT_STRING,
   logging: false,
   dialectOptions: {
     connectString: process.env.DB_CONNECT_STRING,
-    configDir: process.env.TNS_ADMIN,
-    walletLocation: process.env.TNS_ADMIN,
-    walletPassword: process.env.WALLET_PASSWORD,
   },
 });
 
