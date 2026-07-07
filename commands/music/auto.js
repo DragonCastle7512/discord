d98942fbd9b6a85d6f252624f8979b4568e76a35
DragonCastle7512 <kimys75128386@gmail.com>
2026-07-07 15:56:57 +0900

    feat: auto 명령어 라벨 매핑에 서버 및 개인 추천 곡 추가

--- commands/music/auto.js
+++ commands/music/auto.js
@@ -47,7 +47,8 @@ module.exports = {
         '비오는 날': '비오는 날 어울리는 노래',
         '카페': ' 카페 분위기 BGM',
         '우울한': '우울할 때 듣는 위로송',
-        '추천 곡': '추천 곡 (최근 들은 곡 기반)',
+        '서버 추천 곡': '서버 추천 곡 (서버 인기 기반)',
+        '내 추천 곡': '내 추천 곡 (개인 감상 기록 기반)',
       };
 
       const selectedLabel = moodLabelMap[activeMood] || activeMood;
